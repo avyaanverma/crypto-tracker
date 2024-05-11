5233583fc8fce1d21e6f14a858be99e01f4e2606
Avyaan Verma <avyaanver@gmail.com>
2024-05-11 19:21:07 +0530

    prod updates

--- server/index.js
+++ server/index.js
@@ -16,6 +16,11 @@ const app = express();
 // app.use(cors(corsOptions));
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 //database connection
 mongoose.connect(process.env.MONGO_URL)
